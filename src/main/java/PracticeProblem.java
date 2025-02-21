9ab350f71e5b75a3e45305d3e1484f2db6e07af4
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-21 16:19:25 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,51 @@ public class PracticeProblem {
 
 	}
 
+	public static String pyramid(int rows) {
+		StringBuilder current = new StringBuilder();
+		for (int i = 0; i < rows; i++) {
+			for (int e = 0; e < rows - 1 - i; e++) {
+				current.append(" ");
+			}
+			current.append("*");
+			for (int e = 0; e < i; e++) {
+				current.append(" *");
+			}
+			if (i < rows - 1) {
+				current.append("\n");
+			}
+		}
+		return current.toString();
+	}
+
+	public static String square(int length) {
+		StringBuilder current = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			current.append("*");
+		}
+		for (int i = 1; i < length - 1; i++) {
+			current.append("\n");
+			current.append("*");
+			for (int e = 1; e < length - 1; e++) {
+				current.append(" ");
+			}
+			current.append("*");
+		}
+		if (length > 1) {
+			current.append("\n");
+			for (int i = 0; i < length; i++) {
+				current.append("*");
+			}
+		}
+		return current.toString();
+	}
+
+	public static boolean hasLowercase(String check) {
+		for (int i = 0; i < check.length(); i++) {
+			if (check.charAt(i) >= 'a' && check.charAt(i) <= 'z') {
+				return true;
+			}
+		}
+		return false;
+	}
 }
